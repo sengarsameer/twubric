81bf9a54f3619978d615d8a79288715e68cabd9f
SAMEER SENGAR <sengar.sameer@gmail.com>
2018-02-08 22:37:53 +0530

    final update

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
  return [
     //
-    'consumer_key'      => '',
-    'consumer_secret'   => '',
+    'consumer_key'      => 'Your_twitter_consumer_key',
+    'consumer_secret'   => 'Your_twitter_consumer_secret_key',
  
     //
-    'url_login'         => '',
-    'url_callback'      => '',
-    'klout_key' => '',
+    'url_login'         => 'Your_starting_or_landing_page',
+    'url_callback'      => 'Your_destination_page',
+    'klout_key'         => 'Your_klout_key',
 ];
 ?>
